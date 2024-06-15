e2fc9a74e766315b170c0509ae11f0918e245ade
AlstonH <168251888+AlstonH@users.noreply.github.com>
2024-06-15 07:59:12 +0000

    modify

--- streamWeb/index14.py
+++ streamWeb/index14.py
@@ -4,7 +4,9 @@ st.title("計數器範例")
 if 'count' not in st.session_state:             #一開始檢查cookies裡面有沒有, 沒有則給定值0
     st.session_state.count = 0
 
-increment = st.button("Increment")      
+#利用session_state的寫法
+increment = st.button("Increment", key='Increment')      #如若未給key值, 程式會自動給定一個值
+st.session_state                                         #顯示出值
 if increment:
     st.session_state.count += 1                 #每次都加1, 會存入cookies裡面
     
