7f63663d888417debc53cbfa512531df18329cbc
AlstonH <168251888+AlstonH@users.noreply.github.com>
2024-06-21 14:15:44 +0000

    modify

--- Homework BMI_Web/Cal_BMI_Web.py
+++ Homework BMI_Web/Cal_BMI_Web.py
@@ -2,16 +2,38 @@ import streamlit as st
 
 st.image("https://health99.hpa.gov.tw/storage/images/materials/21752.jpg")
 st.header("計算您的BMI:")
-st.subheader("BMI計算公式: 體重(kg)/身高平方(cm)")
-
+st.markdown("BMI計算公式: 體重(kg) / (身高(m)^2)")
 
 with st.form("data"):
     name = st.text_input("請輸入您的姓名:")
     height = st.text_input("請輸入您的身高(cm):")
     weight = st.text_input("請輸入您的體重(kg):")
     
-    st.form_submit_button("送出")
-st.write("您的姓名:",name,"您的身高:",height,"您的體重:",weight)
+    submit = st.form_submit_button("送出")
+    
+def cal_bmi(bmi):
+    if bmi < 18.5:
+        return "您的BMI為體重過輕, 你需要再吃營養些，讓自己重一些！"
+    elif bmi >= 18.5 and bmi < 24:
+        return "您的BMI為健康體位, 很不錯喔，很標準，請您繼續保持！"
+    elif bmi >= 24 and bmi < 27:
+        return "您的BMI為過重, 您得控制一下飲食了，請加油！"
+    elif bmi >= 27 and bmi < 30:
+        return "您的BMI為輕度肥胖, 肥胖容易引起疾病，您得要多多注意自己的健康囉！"
+    elif bmi >= 30 and bmi < 35:
+        return "您的BMI為中度肥胖, 肥胖容易引起疾病，您得要多多注意自己的健康囉！"
+    else:
+        return "您的BMI為重度肥胖, 肥胖容易引起疾病，您得要多多注意自己的健康囉！"
 
+if submit:
+    try:
+        height = float(height) / 100
+        weight = float(weight)
+        bmi = round(weight/height**2,ndigits=2)
+        result = cal_bmi(bmi)
+        st.write("您的姓名:",name,"您的身高:",height,"您的體重:",weight, "BMI", bmi)
+        st.write(result)
+    except ValueError:
+        st.error("請確保您輸入的是數字")
 
 
