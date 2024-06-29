9694495752023d75dbfaff601a10f178dcfd69b7
AlstonH <168251888+AlstonH@users.noreply.github.com>
2024-06-29 01:51:07 +0000

    modify

--- lesson12/index.py
+++ lesson12/index.py
@@ -1,31 +1,17 @@
 import requests
 import streamlit as st
 from pydantic import BaseModel,Field,RootModel,field_validator,field_serializer
+import source
+from source import Root
 
-class Site(BaseModel):
-    站點名稱:str = Field(alias="sna")
-    行政區:str = Field(alias="sarea")
-    日期時間:str = Field(alias="mday")
-    地址:str = Field(alias="ar")
-    總數:int = Field(alias="tot")
-    可借:int = Field(alias="sbi")
-    可還:int = Field(alias="bemp")
-    狀態:bool = Field(alias="act")
-    緯度:float = Field(alias="lat")
-    經度:float = Field(alias="lng")
+data_str = source.download_youbike()
+root = Root.model_validate_json(data_str)
+data = root.model_dump()
 
-class Root(RootModel):
-    root:list[Site]
-    
-@st.cache_data
-def download_youbike() -> str:
-    youbike_url = "https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?size=2000"
-    try:
-        response = requests.get(youbike_url)
-    except Exception as e:
-        print(e)
-    else:
-        return response.text
+def ijk(value):
+    return value['行政區']
 
-data_str = download_youbike()
-Root.model_
+areas:list[str] = list(set(map(ijk,data)))
+
+option = st.selectbox("請選擇行政區",areas)
+st.write("您選擇:", option)
