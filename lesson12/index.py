0cfa56dea18ca724dba0222479d247667993350e
AlstonH <168251888+AlstonH@users.noreply.github.com>
2024-06-29 06:44:13 +0000

    modify

--- lesson12/index.py
+++ lesson12/index.py
@@ -3,6 +3,7 @@ import streamlit as st
 from pydantic import BaseModel,Field,RootModel,field_validator,field_serializer
 import source
 from source import Root
+import pandas as pd
 
 try:
     data_str = source.download_youbike()
@@ -13,13 +14,20 @@ else:
     data = root.model_dump()
     areas:list[str] = list(set(map(lambda value:value['行政區'],data)))
     
+    st.title("台北市youbike各行政區站點資料")
+    tableContainer = st.container()
+    
     def area_change():
-        st.write(st.session_state.sarea)
-        
-    if 'sarea' not in st.session_state:
-        st.session_state.sarea = "淡水區"
+        sarea_name = st.session_state.sarea
+        display_data = []
+        for item in data:
+            if item['行政區'] == sarea_name:
+                display_data.append(item)
+        with tableContainer:
+            df1 = pd.DataFrame(display_data,
+                                columns=['站點名稱','日期時間','地址','總數','可借','可還'])
+            st.dataframe(df1)
         
     with st.sidebar:
         st.selectbox(":purple[請選擇行政區:]",options=areas,on_change=area_change,key='sarea')
     
-    st.write(st.session_state)
