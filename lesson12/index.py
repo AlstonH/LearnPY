80d2700767c44a5c5b468a752fc1d66469c2b342
AlstonH <168251888+AlstonH@users.noreply.github.com>
2024-06-29 03:58:08 +0000

    modify

--- lesson12/index.py
+++ lesson12/index.py
@@ -4,14 +4,22 @@ from pydantic import BaseModel,Field,RootModel,field_validator,field_serializer
 import source
 from source import Root
 
-data_str = source.download_youbike()
-root = Root.model_validate_json(data_str)
-data = root.model_dump()
-
-def ijk(value):
-    return value['行政區']
-
-areas:list[str] = list(set(map(ijk,data)))
-
-option = st.selectbox("請選擇行政區",areas)
-st.write("您選擇:", option)
+try:
+    data_str = source.download_youbike()
+except Exception as e:
+    st.error(e)
+else:
+    root = Root.model_validate_json(data_str)
+    data = root.model_dump()
+    areas:list[str] = list(set(map(lambda value:value['行政區'],data)))
+    
+    def area_change():
+        st.write(st.session_state.sarea)
+        
+    if 'sarea' not in st.session_state:
+        st.session_state.sarea = "淡水區"
+        
+    with st.sidebar:
+        st.selectbox(":purple[請選擇行政區:]",options=areas,on_change=area_change,key='sarea')
+    
+    st.write(st.session_state)
